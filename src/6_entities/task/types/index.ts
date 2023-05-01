3ddf0ebec2beb07708e3598bbae1a177e5b54d03
Vitaliy Tikhonov <vitaliy.a.tikhonov@yandex.ru>
2023-05-01 23:13:26 +0300

    Amended the types

--- src/6_entities/task/types/index.ts
+++ src/6_entities/task/types/index.ts
@@ -1,8 +1,9 @@
 export interface ITask {
   id: string;
+  name: string;
   type: TaskType;
   parentId?: string;
-  subtasks?: string[];
+  subtasks?: string[]; // for STAGED
 
   capacity: number; // hours
   priority: string;
@@ -10,18 +11,18 @@ export interface ITask {
   deadline: string; // timestamp; fixedEnd: boolean;
 
   status: TaskStatus;
-  notes: string[];
   started: string; // timestamp
   edited: string; // timestamp
   closed: string; // timestamp
   spent: number; // hours
+  notes: string[];
 }
 
 enum TaskType {
-  REGULAR = "REGULAR",
-  ONE_OFF = "ONE_OFF",
-  STAGED = "STAGED",
-  CONTINUOUS = "CONTINUOUS",
+  REGULAR = "REGULAR", // Do the cleaning, Take shower, Back up the data. Like the staged but where it is not feasible to single out individual stages
+  ONE_OFF = "ONE_OFF", // Study a topic from the list for my professional development
+  STAGED = "STAGED", // THE ONLY ONE THAT TAKES SUBTASKS. Study topics per the list for my professional development, Get a visa (multi-step process)
+  CONTINUOUS = "CONTINUOUS", // Read Book Grokking Algorhithms, Fulfill Course on Python
 }
 
 enum TaskStatus {
